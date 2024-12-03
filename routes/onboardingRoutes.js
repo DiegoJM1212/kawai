c03ca8b33541cb22400f19c0362c2d74b308de61
DiegoJM1212 <162833103+DiegoJM1212@users.noreply.github.com>
2024-12-03 13:07:23 -0600

    Update onboardingRoutes.js

--- routes/onboardingRoutes.js
+++ routes/onboardingRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { checkOnboarding, showOnboarding } = require('../controladores/onboardingController');
 
-// Ruta raíz: Comprueba si el usuario ha visto el onboarding
-router.get('/', checkOnboarding, (req, res) => {
+// Ruta raíz: Muestra el onboarding directamente
+router.get('/', showOnboarding);
+
+// Ruta para el login
+router.get('/login', checkOnboarding, (req, res) => {
   res.render('login');  // Muestra el login si ya pasó el onboarding
 });
-
-// Ruta para mostrar el onboarding
-router.get('/onboarding', showOnboarding);
 
 module.exports = router;
