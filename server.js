803cf8a6274c294f3967b45a49d28b5ab34803e0
DiegoJM1212 <162833103+DiegoJM1212@users.noreply.github.com>
2024-12-03 01:51:45 -0600

    Update server.js

--- server.js
+++ server.js
@@ -151,10 +151,9 @@ app.get('/banco', (req, res) => {
 });
 
 app.get('/mascotas', (req, res) => {
-    const apiUrl = process.env.API2_URL || 'https://adop.onrender.com';
-    console.log('API2_URL being sent to client:', apiUrl); // Log the API URL
-    res.render(path.join(__dirname, 'html', 'mascotasapi.html'), { apiUrl: apiUrl });
+    res.render('mascotasapi', { apiUrl: 'https://adop.onrender.com' });
 });
+
 // Ruta para 'recuperarpass.html'
 app.get('/recuperar', (req, res) => {
     res.sendFile(path.join(__dirname, 'html', 'recuperarpass.html'));
