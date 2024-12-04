cd54a010e128b2cdb1db62285571fc5abeae47be
DiegoJM1212 <162833103+DiegoJM1212@users.noreply.github.com>
2024-12-04 15:43:41 -0600

    Update paypalroute.js

--- routes/paypalroute.js
+++ routes/paypalroute.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const paypalController = require('../controladores/paypal');
 
-// Ruta para crear un pago con PayPal
-router.post('/crear-pago-paypal', paypalController.crearPagoPaypal);
-
-// Ruta para ejecutar el pago de PayPal después de la aprobación
-router.post('/ejecutar-pago-paypal', paypalController.ejecutarPagoPaypal);
+router.post('/crear-pago', paypalController.createPayment);
 
 module.exports = router;
+
