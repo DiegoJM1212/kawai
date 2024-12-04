4b221ba0a6bf2ebef9840cb5ff63bb6c6bbb775b
DiegoJM1212 <162833103+DiegoJM1212@users.noreply.github.com>
2024-12-04 15:53:39 -0600

    Update appointmentRoutes.js

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
-// Asegúrate de que la ruta al controlador sea correcta
 const appointmentController = require('../controladores/appointmentController');
 
 // Ruta para agendar cita veterinaria
-router.post('/agendar', (req, res) => {
-  if (typeof appointmentController.agendarCita === 'function') {
-    appointmentController.agendarCita(req, res);
-  } else {
-    res.status(500).send('Error: Función agendarCita no definida.');
-  }
-});
+router.post('/agendar', appointmentController.agendarCita);
 
 // Nueva ruta para comprobar disponibilidad
-router.post('/comprobar-disponibilidad', (req, res) => {
-  if (typeof appointmentController.comprobarDisponibilidad === 'function') {
-    appointmentController.comprobarDisponibilidad(req, res);
-  } else {
-    res.status(500).send('Error: Función comprobarDisponibilidad no definida.');
-  }
-});
+router.post('/comprobar-disponibilidad', appointmentController.comprobarDisponibilidad);
 
 module.exports = router;
